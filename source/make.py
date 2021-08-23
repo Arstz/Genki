a84f9803237ae564b8c0683a2d9815b59a7f9508
Pengyss <32GGHz@gmail.com>
2021-08-24 00:23:55 +0300

    piton

--- source/make.py
+++ source/make.py
@@ -1,4 +1,25 @@
 import os
 
+src = ""
+
 filenames = os.listdir(".")
+dirnames = list(filter (lambda x : not '.' in x, filenames))
+for i in dirnames:
+    for j in os.listdir(i):
+        filenames.append(i + "/" + j)
+
+for i in filenames:
+    i = "./" + i
+
+filenames = list(filter(lambda x : ".h" in x or ".cpp" in x or ".glslv" in x or ".glslf" in x, filenames))
 print(filenames)
+
+for i in filenames:
+    file = open(i, "r")
+    for line in file.readlines():
+        src += line
+    file.close()
+
+file = open("src.txt", "w")
+file.write(src)
+file.close()
